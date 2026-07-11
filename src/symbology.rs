2f6772c315f1a5b6eb5c523979cf880502cdb754
Mark Karpeles <magicaltux@gmail.com>
2026-07-12 02:40:28 +0900

    Mark GS1 DataBar Omni/Limited implemented

--- src/symbology.rs
+++ src/symbology.rs
@@ -241,7 +241,18 @@ impl Symbology {
         use Symbology::*;
         matches!(
             self,
-            QrCode | DataMatrix | Code128 | Gs1_128 | Ean13 | Ean8 | UpcA | UpcE | Ean2 | Ean5
+            QrCode
+                | DataMatrix
+                | Code128
+                | Gs1_128
+                | Ean13
+                | Ean8
+                | UpcA
+                | UpcE
+                | Ean2
+                | Ean5
+                | DataBarOmni
+                | DataBarLimited
         )
     }
 
